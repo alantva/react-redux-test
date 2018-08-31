935fa6a062f2a1f168a06bd47f607e13e12e6699
Alan Oliveira <alan_tva_90@yahoo.com.br>
2018-08-31 12:55:14 -0300

    feat: tab container

---
+++ src/containers/Tab.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import ListContainer from './List';
+
+const INITIAL_STATE = { value: 0 };
+
+const styles = theme => ({
+  tab: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    minWidth: 400,
+    flexWrap: 'wrap',
+    margin: theme.spacing.unit * 2,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: theme.palette.primary.main,
+    color: theme.palette.primary.main
+  }
+});
+
+class CommonTab extends React.Component {
+  state = INITIAL_STATE;
+
+  static defaultProps = {
+    data: []
+  };
+
+  static propTypes = {
+    data: PropTypes.arrayOf(PropTypes.object),
+    classes: PropTypes.object.isRequired
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes, data } = this.props;
+    const { value } = this.state;
+
+    return (
+      <div className={classes.tab}>
+        <AppBar position="static">
+          <Tabs value={value} onChange={this.handleChange}>
+            <Tab label="Completas" />
+            <Tab label="Incompletas" />
+          </Tabs>
+        </AppBar>
+        <ListContainer title="Lista de Tarefas" data={data.filter(d => d.complete === !value)} />
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(CommonTab);
